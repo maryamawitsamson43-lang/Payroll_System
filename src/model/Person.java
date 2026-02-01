3f8efc4cbf328b4a444d4c86cd61981c03f54d6f
Maryamawit Samson <maryamawitsamson43@gmail.com>
2026-02-01 13:12:04 +0300

    Fix formatting of constructor in Person class

--- src/model/Person.java
+++ src/model/Person.java
@@ -5,7 +5,7 @@ public abstract class Person {
     private String personID;
     private String password;
 
-    public Person(String name, String personID, String password) {
+    public Person(String name, String personID, String password)  {
         this.name = name;
         this.personID = personID;
         this.password = password;
